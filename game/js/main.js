1083f6b17cb35285a1cdeb3e92e68c62de9f5cbb
Jake Miller <hillsighed@gmail.com>
2016-12-12 12:39:35 -0800

    Creates board excludes coloring tiles

--- game/js/main.js
+++ game/js/main.js
@@ -1,1 +1,27 @@
 console.log('Linked!');
+
+
+// Create Game Board
+var createGameBoard = function() {
+  $table = document.createElement('table');
+  $table.setAttribute('id', 'table');
+  document.body.appendChild($table);
+
+  // Create Table Rows 8 of them
+  for (var i = 0; i < 8 ; i++) {
+    $tableRow = document.createElement('tr');
+    $tableRow.textContent = " ";
+    $table.appendChild($tableRow);
+  };
+
+  // Get Table Rows
+  $allTableRows = document.querySelectorAll('tr');
+
+  for (var i = 0; i < $allTableRows.length ; i++) {
+      for (var j = 0; j < 8; j++) {
+        $tableCell = document.createElement('td');
+        $tableCell.classList.add('tableCell');
+        $allTableRows[i].appendChild($tableCell);
+      };
+  };
+};
